d5dcb6b8f7d4f6e3cab96642a6f45eee38a0eb6c
Jalp Patel <jalpunpatel95@gmail.com>
2026-03-27 07:05:29 +0530

    blank screen issue

--- src/index.js
+++ src/index.js
@@ -5,9 +5,8 @@ import { BrowserRouter } from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
- const basename = process.env.PUBLIC_URL || "/";
 root.render(
-    <BrowserRouter basename={basename}> 
+    <BrowserRouter> 
       <App />
     </BrowserRouter>
   );
